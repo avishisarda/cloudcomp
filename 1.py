00b6c3bf05ab51051d3062d8d81d1707bc5e0277
avishisarda <119294289+avishisarda@users.noreply.github.com>
2024-08-24 17:14:37 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,5 +11,8 @@ plt.xlabel("height")
 plt.ylabel("weight")
 reg=linear_model.LinearRegression()
 reg.fit(height,weight)
-X_height=[[12.0]]
-print(reg.predict(X_height))
+#X_height=[[12.0]]
+#print(reg.predict(X_height))
+X_height=input()
+z = [[X_height]]
+print(reg.predict(z))
